2e1797015590a93a22a62c66f9192c844adfc564
alperenozdnc <alperenozdincc@gmail.com>
2025-10-14 19:41:22 +0300

    refactor: check if file exists before editing

--- src/main.c
+++ src/main.c
@@ -1,3 +1,6 @@
+#include "utils/file_exists.h"
+
+#include <stdbool.h>
 #include <stdio.h>
 
 int main(int argc, char **argv) {
@@ -7,6 +10,14 @@ int main(int argc, char **argv) {
         return 1;
     } else if (argc > 2) {
         printf("ERROR: only one file allowed\n");
+
+        return 1;
+    }
+
+    const char *FILENAME = argv[1];
+
+    if (!file_exists(FILENAME)) {
+        printf("ERROR: %s doesnt exist\n", FILENAME);
 
         return 1;
     }
