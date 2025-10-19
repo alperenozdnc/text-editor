18579c6683d8fc97e94777c14433dcddd6d6c22f
alperenozdnc <alperenozdincc@gmail.com>
2025-10-19 23:45:24 +0300

    feat: print line func

---
+++ src/utils/printl.c
@@ -0,0 +1,21 @@
+#include "printl.h"
+
+#include <stdio.h>
+#include <string.h>
+
+void printl(char lines[MAX_ROW_SIZE][MAX_COL_SIZE]) {
+    for (size_t i = 0; i < strlen(*lines); i++) {
+        char *line = lines[i];
+
+        for (size_t j = 0; j < strlen(line); j++) {
+            char c = line[j];
+
+            if (c != '\n') {
+                printf("%c", c);
+                continue;
+            }
+
+            printf("\r\n");
+        }
+    }
+}
---
+++ src/utils/printl.h
@@ -0,0 +1,11 @@
+#define MAX_ROW_SIZE 1000
+#define MAX_COL_SIZE 1000
+
+#ifndef PRINTL_H
+#define PRINTL_H
+
+#include <stdbool.h>
+
+void printl(char lines[MAX_ROW_SIZE][MAX_COL_SIZE]);
+
+#endif
