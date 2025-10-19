1b29a3bbbefa364434c4ebb658e1149e62e440c4
alperenozdnc <alperenozdincc@gmail.com>
2025-10-19 23:44:59 +0300

    feat: arg validation abstraction

---
+++ src/utils/is_valid_args.c
@@ -0,0 +1,24 @@
+#include "file_exists.h"
+
+#include <stdbool.h>
+#include <stdio.h>
+
+bool is_valid_args(int argc, const char *FILENAME) {
+    if (argc < 2) {
+        printf("ERROR: no input files\n");
+
+        return false;
+    } else if (argc > 2) {
+        printf("ERROR: only one file allowed\n");
+
+        return false;
+    }
+
+    if (!file_exists(FILENAME)) {
+        printf("ERROR: %s doesnt exist\n", FILENAME);
+
+        return false;
+    }
+
+    return true;
+}
---
+++ src/utils/is_valid_args.h
@@ -0,0 +1,8 @@
+#ifndef IS_VALID_ARGS_H
+#define IS_VALID_ARGS_H
+
+#include <stdbool.h>
+
+bool is_valid_args(int argc, const char *FILENAME);
+
+#endif
