07480d461b45eacbd4e7dbfcaaceae9008816838
alperenozdnc <alperenozdincc@gmail.com>
2025-10-20 20:39:17 +0300

    chore: no unused

--- src/utils/printl.c
+++ src/utils/printl.c
@@ -4,7 +4,6 @@
 void printl(char lines[MAX_ROW_SIZE][MAX_COL_SIZE], size_t size) {
     for (size_t i = 0; i < size; i++) {
         char *line = lines[i];
-        char *str = "";
 
         for (size_t j = 0; j < strlen(line); j++) {
             char c = line[j];
