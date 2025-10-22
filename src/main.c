53be1d4086ee1095ba594118df821418273d8a4c
alperenozdnc <alperenozdincc@gmail.com>
2025-10-22 22:21:55 +0300

    refactor: header file for main script

--- src/main.c
+++ src/main.c
@@ -1,21 +1,4 @@
-#include "include/is_valid_args.h"
-#include "include/printl.h"
-
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-#define mvcurs(x, y) printf("\033[%d;%dH", y, x)
-
-#define set_raw() system("stty raw -echo")
-#define set_canonical() system("stty cooked echo")
-
-#define KEY_ESC '\e'
-#define ARROW_UP 'k'
-#define ARROW_DOWN 'j'
-#define ARROW_LEFT 'h'
-#define ARROW_RIGHT 'l'
+#include "include/main.h"
 
 typedef struct {
     int x;
