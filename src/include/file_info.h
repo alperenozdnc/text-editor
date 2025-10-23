de3e83108fe246eb966e8b44983a6c9be078cf4c
alperenozdnc <alperenozdincc@gmail.com>
2025-10-23 22:33:15 +0300

    feat: file info struct

---
+++ src/include/file_info.h
@@ -0,0 +1,21 @@
+#ifndef FILE_INFO_H
+#define FILE_INFO_H
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    const char *path;
+    FILE *fptr;
+    char **lines;
+    int line_count;
+} file_info;
+
+#define MAX_ROW_SIZE 1000
+
+void init_file_info(file_info *file, const char *path);
+void free_file_info(file_info *file);
+
+#endif
---
+++ src/utils/file_info.c
@@ -0,0 +1,29 @@
+#include "../include/file_info.h"
+#include "../include/main.h"
+
+void init_file_info(file_info *file, const char *path) {
+    file->path = path;
+    file->fptr = fopen(file->path, "r");
+    file->lines = NULL;
+    file->line_count = 0;
+
+    char buffer[MAX_ROW_SIZE];
+
+    while (fgets(buffer, sizeof(buffer), file->fptr)) {
+        file->lines =
+            realloc(file->lines, (file->line_count + 1) * sizeof(char *));
+
+        file->lines[file->line_count] = strdup(buffer);
+        file->line_count++;
+    }
+}
+
+void free_file_info(file_info *file) {
+    fclose(file->fptr);
+
+    for (int i = 0; i < file->line_count; i++) {
+        free(file->lines[i]);
+    }
+
+    free(file->lines);
+}
