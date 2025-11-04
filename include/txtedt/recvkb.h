648ba109d65eae87ebe87caec8770524717ff08b
alperenozdnc <alperenozdincc@gmail.com>
2025-11-04 20:24:08 +0300

    chore: use new util

--- include/txtedt/recvkb.h
+++ include/txtedt/recvkb.h
@@ -1,12 +1,13 @@
 #ifndef RECVKB_H
 #define RECVKB_H
 
+#include <txtedt/ctrl.h>
 #include <txtedt/cursor.h>
 #include <txtedt/file_info.h>
 #include <txtedt/terminal.h>
 
 #define KEY_ESC '\e'
-#define KEY_EXIT 3
+#define KEY_EXIT ctrl('c')
 #define KEY_BACKSPACE 127
 #define KEY_ENTER '\r'
 
